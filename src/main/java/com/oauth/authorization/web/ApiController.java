3d434eff717473f8456eeb80014633b2f7a7c7de
brollins90 <brollins90@gmail.com>
2015-12-11 09:31:27 -0700

    i dont remember

--- src/main/java/com/oauth/authorization/web/ApiController.java
+++ src/main/java/com/oauth/authorization/web/ApiController.java
@@ -1,21 +1,17 @@
 package com.oauth.authorization.web;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.oauth.authorization.domain.AccessToken;
 import com.oauth.authorization.domain.User;
 import com.oauth.authorization.service.AccessTokenService;
 import com.oauth.authorization.service.ClientService;
 import com.oauth.authorization.service.UserAuthenticationTokenManager;
 import com.oauth.authorization.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class ApiController {
@@ -27,27 +23,29 @@ public class ApiController {
     private ClientService clientService;
 
     @Autowired
-    private UserAuthenticationTokenManager userAuthenticationTokenManager;
-    
-    @Autowired
     private UserService userService;
 
 
     @RequestMapping(path = "/api/user", method = {RequestMethod.GET, RequestMethod.POST})
     public ResponseEntity authorize(
-            @RequestParam(value = "access_token", required = false) String access_token,
+            @RequestHeader(value = "Authorization", required = false) String bearer_token,
             HttpServletRequest request) {
 
-        if (access_token == null) {
+        String tok = bearer_token.substring(7);
+        System.out.println("/api/user?bearer_token=" + tok);
+        if (bearer_token == null) {
             return new ResponseEntity(HttpStatus.FORBIDDEN);
         }
-        AccessToken realToken = accessTokenService.findByAccessToken(access_token);
-        if(realToken.getClientId().equals("shayne2") &&
-        		realToken.getUsername().equals("shayne") &&
-        		realToken.getScope().equals("email")) {
-        	User user = userService.findByUsername("shayne");
-        	return new ResponseEntity(user.getEmail(), HttpStatus.OK);
+
+        AccessToken realToken = accessTokenService.findByAccessToken(tok);
+        if (realToken != null) {
+            User u = userService.findByUsername(realToken.getUsername());
+            ApiResponse resp = new ApiResponse();
+            if (realToken.getScope().indexOf("username") != -1) resp.username = u.getUsername();
+            if (realToken.getScope().indexOf("email") != -1) resp.email = u.getEmail();
+            if (realToken.getScope().indexOf("phone") != -1) resp.phone = u.getPhone();
+            return new ResponseEntity(resp, HttpStatus.OK);
         }
-        return new ResponseEntity("You are Cool", HttpStatus.OK);
+        return new ResponseEntity(HttpStatus.FORBIDDEN);
     }
 }
